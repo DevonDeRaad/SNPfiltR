bf5fccb296d18ccb81653ac67849e2856fa4bb44
Devon DeRaad <devonderaad@gmail.com>
2022-03-23 17:20:45 -0500

    add package startup message

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+
+#print package startup message
+packageStartupMessage(
+  c("This is SNPfiltR v.1.0.0 \n\n",
+  "If you use SNPfiltR in your published work, please cite: \n\n",
+  "DeRaad, D.A. 2021. SNPfiltR: an R package for interactive and reproducible SNP filtering. Preprint on Authorea. <http://dx.doi.org/10.22541/au.163976415.53888836/v1> \n\nand \n\n",
+  "Knaus, Brian J., and Niklaus J. Grunwald. 2017. VCFR: a package to manipulate and visualize variant call format data in R. Molecular Ecology Resources 17.1:44-53. http://dx.doi.org/10.1111/1755-0998.12549.")
+  )
+
