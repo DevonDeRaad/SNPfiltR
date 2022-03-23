a0e5ea14589073d57897c3be804ce5993950b98e
Devon DeRaad <devonderaad@gmail.com>
2022-03-23 17:44:19 -0500

    Update package message

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,12 @@
 
 #print package startup message
+.onAttach <- function(libname, pkgname) {
+  pkg.version <- utils::packageVersion("SNPfiltR")
 packageStartupMessage(
-  c("This is SNPfiltR v.1.0.0 \n\n",
-  "If you use SNPfiltR in your published work, please cite: \n\n",
-  "DeRaad, D.A. 2021. SNPfiltR: an R package for interactive and reproducible SNP filtering. Preprint on Authorea. <http://dx.doi.org/10.22541/au.163976415.53888836/v1> \n\nand \n\n",
+  c(paste0("This is SNPfiltR v.", pkg.version, "\n\n"),
+  "Detailed usage information is available at: devonderaad.github.io/SNPfiltR/ \n\n",
+  "If you use SNPfiltR in your published work, please cite the following papers: \n\n",
+  "DeRaad, D.A. 2021. SNPfiltR: an R package for interactive and reproducible SNP filtering. Preprint on Authorea. <http://dx.doi.org/10.22541/au.163976415.53888836/v1> \n\n",
   "Knaus, Brian J., and Niklaus J. Grunwald. 2017. VCFR: a package to manipulate and visualize variant call format data in R. Molecular Ecology Resources 17.1:44-53. http://dx.doi.org/10.1111/1755-0998.12549.")
   )
-
+}
